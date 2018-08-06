99bd14bb8d772461d4624e33715db3e6a8c6596e
Ariel Tuley <ariel.sarah@gmail.com>
2018-08-06 13:42:26 -0500

    add amount to grain

--- Models/Grain.cs
+++ Models/Grain.cs
@@ -7,5 +7,6 @@ namespace Models.Grain
         public int GrainId {get; set;}
         public string GrainName {get; set;}
         public List<Horse> Horses {get; set;}
+        public decimal amount {get; set;}
     }
 }
